773e6d87ad7e89f4cca5c7e5da693383614053d6
chenjianhui <2294198058@qq.com>
2023-11-14 02:40:16 +0800

    init

---
+++ user/user-start/src/main/java/com/huahuo/user/nacosdiscovery/NacosDiscoveryConfiguration.java
@@ -0,0 +1,9 @@
+package com.huahuo.user.nacosdiscovery;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@EnableDiscoveryClient
+@Configuration
+public class NacosDiscoveryConfiguration {
+}

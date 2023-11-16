a8e019203d6b296f600b97f541503c440923f8f9
chenjianhui <2294198058@qq.com>
2023-11-16 22:08:40 +0800

    整合sentinel rocketmq

--- item/item-web/src/main/java/com/huahuo/item/controller/ItemController.java
+++ item/item-web/src/main/java/com/huahuo/item/controller/ItemController.java
@@ -6,14 +6,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @RestController
 @RequestMapping("/item")
 public class ItemController {
-    @Autowired
-    UserFeignService userFeignService;
+  @Autowired
+  UserFeignService userFeignService;
 
-    @GetMapping("/echo")
-    String echo() {
-        return userFeignService.echo();
-    }
+  @GetMapping("/echo")
+  String echo() {
+    return userFeignService.echo();
+  }
+
+  @GetMapping("/store")
+  void storeSession(HttpServletRequest session) {
+    session.getSession().setAttribute("code", "6666");
+  }
+
+  @GetMapping("/get/code")
+  Object getCode(HttpSession session) {
+    System.out.println(session.getAttribute("code"));
+    return session.getAttribute("code");
+  }
+
+  @GetMapping("/test/sentinel")
+  void testSentinel() {
+
+  }
 }
